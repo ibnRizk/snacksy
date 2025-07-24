8decc57237451c457d68c8133394f3cbec1f4719
Hussein Rizk <husseinrizk117@gmail.com>
2025-07-24 11:03:00 +0300

    finish app header

--- lib/widgets/search_area.dart
+++ lib/widgets/search_area.dart
@@ -1,31 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SearchArea extends StatelessWidget {
   const SearchArea({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide.none,
+    return Padding(
+      padding: const EdgeInsets.all(18),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  hintText: "Search",
+                  prefixIcon: Icon(Icons.search),
                 ),
-                hintText: "Search",
-                prefixIcon: Icon(Icons.search),
               ),
             ),
           ),
-        ),
-      ],
+          SizedBox(width: 10),
+          InkWell(
+            child: Container(
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+              ),
+              child: SvgPicture.asset(
+                'assets/filter-svgrepo-com.svg',
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
